6f34e35e1a8da84dbbc596afe731ad2505b5ef13
John Goff <john.g@hiredhippo.ai>
2024-04-29 10:17:38 -0400

    Write text at application start up

--- lib/kobo_daily_notes/application.ex
+++ lib/kobo_daily_notes/application.ex
@@ -7,14 +7,20 @@ defmodule KoboDailyNotes.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: KoboDailyNotes.Worker.start_link(arg)
-      # {KoboDailyNotes.Worker, arg}
-    ]
+    # children = [
+    #   # Starts a worker by calling: KoboDailyNotes.Worker.start_link(arg)
+    #   # {KoboDailyNotes.Worker, arg}
+    # ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: KoboDailyNotes.Supervisor]
-    Supervisor.start_link(children, opts)
+    # # See https://hexdocs.pm/elixir/Supervisor.html
+    # # for other strategies and supported options
+    # opts = [strategy: :one_for_one, name: KoboDailyNotes.Supervisor]
+    # Supervisor.start_link(children, opts)
+    File.write!(
+      Path.join([:code.priv_dir(:kobo_daily_notes), "test.txt"]),
+      "This is text written from elixir"
+    )
+
+    {:error, :no_start}
   end
 end
